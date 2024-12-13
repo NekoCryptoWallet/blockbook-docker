a8e42a8f365706a169e274a9279787c039e4a989
NekoSuneVR <chisdeal2014@hotmail.com>
2024-12-13 17:56:44 +0000

    TEST

--- launch.sh
+++ launch.sh
@@ -2,4 +2,12 @@
 
 CFG_FILE=/blockbook/config/blockchaincfg.json
 
-exec ./blockbook -blockchaincfg=/blockbook/config/blockchaincfg.json -datadir=/blockbook/db -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9141} -logtostderr "$@"
+# Loop to restart blockbook if it fails
+while true; do
+    echo "Starting Blockbook..."
+    ./blockbook -blockchaincfg=$CFG_FILE -datadir=/blockbook/db -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9141} -logtostderr "$@"
+    
+    # If ./blockbook fails, wait before restarting
+    echo "Blockbook crashed with exit code $?. Restarting in 5 seconds..."
+    sleep 5
+done
