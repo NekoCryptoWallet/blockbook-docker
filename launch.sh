4824e378c31a7e490fbf46b058f7e15aa3b44b7a
NekoSuneVR <chisdeal2014@hotmail.com>
2024-12-16 15:56:09 +0000

    asda

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ CFG_FILE=/blockbook/config/blockchaincfg.json
 # Loop to restart blockbook if it fails
 while true; do
     echo "Starting Blockbook..."
-    exec ./blockbook -blockchaincfg=$CFG_FILE -datadir=/blockbook/db -resyncindexperiod=60017 -resyncmempoolperiod=60017 -resyncmempoolperiod=60017 -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9141} -logtostderr "$@"
+    exec ./blockbook -blockchaincfg=$CFG_FILE -datadir=/blockbook/db -resyncindexperiod=60017 -resyncmempoolperiod=60017 -resyncmempoolperiod=60017 -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9149} -logtostderr "$@"
     
     # If ./blockbook fails, wait before restarting
     echo "Blockbook crashed with exit code $?. Restarting in 5 seconds..."
